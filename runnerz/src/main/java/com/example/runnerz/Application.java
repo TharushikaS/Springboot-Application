6202b723907ef3c840a1c41423c648929ba4efc2
R.L.D.T.H. Surasinghe <surasinghe_rldth_e23@engug.ruh.ac.lk>
2025-01-22 22:27:52 +0530

    feat : create Application.java

---
+++ runnerz/src/main/java/com/example/runnerz/Application.java
@@ -0,0 +1,25 @@
+package com.example.runnerz;
+
+import com.example.runnerz.user.UserHttpClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	UserHttpClient userHttpClient() {
+	    RestClient restClient = RestClient.create("https://jsonplaceholder.typicode.com/");
+	    HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
+	    return factory.createClient(UserHttpClient.class);
+	}
+}
